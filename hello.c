aece34087d9f58a8d96760fdecb75e1785f1b103
Carlos Camacho <ccamacho@me.com>
2021-03-15 13:14:04 -0700

    xyz

--- hello.c
+++ hello.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+
+int doSomething(char *name){
+	printf("Doing Something %s\n",name);
+	return (25);
+}
+
 int main(int agrc, char **argv){
    printf("Hello World\n");
    printf("This is test\n");
+   doSomething("Carlos Luis");
 }
+
+
